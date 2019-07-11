c2985d896b34a5b491f3b73c343b04c0ee7c1295
xiliangMa <xiliangMa@outlook.com>
2019-07-11 14:16:02 +0800

    add eureka client
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

--- provider-dept/src/main/java/com/examples/spcloud/DeptProviderApplication.java
+++ provider-dept/src/main/java/com/examples/spcloud/DeptProviderApplication.java
@@ -3,12 +3,14 @@ package com.examples.spcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Create by $(xiliangMa) on 2019-07-10
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan(value = {"com.examples.spcloud.dao"})
 public class DeptProviderApplication {
     public static void main(String[] args) {
