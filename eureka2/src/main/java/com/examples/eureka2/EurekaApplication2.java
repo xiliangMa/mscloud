5c9b37663b5ddceffd72342e18a23d2da1884a55
xiliangMa <xiliangMa@outlook.com>
2019-07-11 14:54:22 +0800

    add eureka server DS Replicas
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

---
+++ eureka2/src/main/java/com/examples/eureka2/EurekaApplication2.java
@@ -0,0 +1,17 @@
+package com.examples.eureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Create by $(xiliangMa) on 2019-07-11
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication2 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication2.class, args);
+    }
+}
