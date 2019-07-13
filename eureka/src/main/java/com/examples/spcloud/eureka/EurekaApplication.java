790aa57837adad4a851e2703ac7d37bae3b2e392
xiliangMa <xiliangMa@outlook.com>
2019-07-13 18:42:10 +0800

    add eureka server
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

---
+++ eureka/src/main/java/com/examples/spcloud/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.examples.spcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Create by $(xiliangMa) on 2019-07-13
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
