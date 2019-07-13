8212d627f601ec1e1981dbf17388b6f3047e5a91
xiliangMa <xiliangMa@outlook.com>
2019-07-14 00:49:32 +0800

    add hystrix dashboard  for feign

--- consumer-feign/src/main/java/com/examples/spcloud/consumer/ConsumerFeignApplication.java
+++ consumer-feign/src/main/java/com/examples/spcloud/consumer/ConsumerFeignApplication.java
@@ -3,6 +3,7 @@ package com.examples.spcloud.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ConsumerFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerFeignApplication.class, args);
---
+++ consumer-feign/src/main/java/com/examples/spcloud/consumer/configuration/HystrixDashboardConfiguration.java
@@ -0,0 +1,25 @@
+package com.examples.spcloud.consumer.configuration;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Create by $(xiliangMa) on 2019-07-14
+ */
+
+@Configuration
+public class HystrixDashboardConfiguration {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet =new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean =new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+
+}
