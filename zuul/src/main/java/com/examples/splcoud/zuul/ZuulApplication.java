036b042d4b289ccd6e36b06aec1e1f93b04a2ed7
xiliangMa <xiliangMa@outlook.com>
2019-07-15 10:53:04 +0800

    add zuul service

---
+++ zuul/src/main/java/com/examples/splcoud/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.examples.splcoud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Create by $(xiliangMa) on 2019-07-15
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
