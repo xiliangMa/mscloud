d6d19ef9c26a4bd38a92ba05c95d5f2a220924e6
xiliangMa <xiliangMa@outlook.com>
2019-07-16 11:33:33 +0800

    add zipkin service

---
+++ zipkin/src/main/java/com/examples/sploucd/zipkin/ZipkinApplication.java
@@ -0,0 +1,19 @@
+package com.examples.sploucd.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * Create by $(xiliangMa) on 2019-07-15
+ */
+
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
