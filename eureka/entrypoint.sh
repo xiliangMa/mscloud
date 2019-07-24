606268975329222942052ea5106ce0c57e7302f3
xiliangMa <xiliangma@outlook.com>
2019-07-24 02:39:54 +0000

    Update entrypoint.sh

--- eureka/entrypoint.sh
+++ eureka/entrypoint.sh
@@ -3,5 +3,20 @@
 # eureka start file
 
 JAR=eureka-0.0.1-SNAPSHOT.jar
+JAR_PATH="/app/"
 
-java -Xmx200m -jar /app/$JAR --spring.profiles.active=dev
+while [[ $# -ge 1 ]]; do
+    case $1 in
+        -JAR_PATH|--JP )
+            JAR_PATH=$2
+            echo $JAR_PATH
+            shift 2
+            ;;
+        * )
+            echo $JAR_PATH
+            shift
+            ;;
+    esac
+done
+
+java -Xmx200m -jar /$JAR_PATH$JAR --spring.profiles.active=dev
