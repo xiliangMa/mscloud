31a9b7335b7c527f6346719ca080abae293da3df
xiliangMa <xiliangma@outlook.com>
2019-07-24 02:55:24 +0000

    Update entrypoint.sh

--- eureka/entrypoint.sh
+++ eureka/entrypoint.sh
@@ -3,20 +3,27 @@
 # eureka start file
 
 JAR=eureka-0.0.1-SNAPSHOT.jar
+MODEL=local
 JAR_PATH="/app/"
+
+# CICD 模式下的路径必须和 .gitlab-ci.yml 下的路径一致
+JAR_PATG_CICD="/tmp/source/eureka/target"
 
 while [[ $# -ge 1 ]]; do
     case $1 in
-        -JAR_PATH|--JP )
-            JAR_PATH=$2
-            echo $JAR_PATH
+        -m|--MODEL )
+            MODEL=$2
+            echo $MODEL
             shift 2
             ;;
         * )
-            echo $JAR_PATH
+            if ["$MODEL" = "cicd"];then
+                JAR_PATH=$JAR_PATG_CICD
+            fi
+            echo "模式为：$MODEL, jar包目录为：$JAR_PATH"
             shift
             ;;
     esac
 done
 
-java -Xmx200m -jar /$JAR_PATH$JAR --spring.profiles.active=dev
+java -Xmx200m -jar $JAR_PATH$JAR --spring.profi les.active=dev
